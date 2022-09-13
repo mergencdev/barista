13a8a5002271f075dee4a01b8317da85641b9d1a
Mehmet Emin Ergenc <mergencdev@gmail.com>
2022-09-13 20:45:04 +0300

    -Center align the ActionBar title

--- app/src/main/java/com/mergenc/barista/MainActivity.kt
+++ app/src/main/java/com/mergenc/barista/MainActivity.kt
@@ -2,10 +2,14 @@ package com.mergenc.barista
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+        supportActionBar?.setCustomView(R.layout.layout_actionbar)
     }
 }
