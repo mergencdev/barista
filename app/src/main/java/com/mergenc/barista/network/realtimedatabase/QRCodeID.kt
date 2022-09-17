d281ef6b7a52c53425e2116c4929fa25d9e52278
Mehmet Emin Ergenc <mergencdev@gmail.com>
2022-09-17 09:43:44 +0300

    -Insert QR IR to Realtime Database

---
+++ app/src/main/java/com/mergenc/barista/network/realtimedatabase/QRCodeID.kt
@@ -0,0 +1,6 @@
+package com.mergenc.barista.network.realtimedatabase
+
+import com.google.firebase.database.IgnoreExtraProperties
+
+@IgnoreExtraProperties
+class QRCodeID(val id: String? = "")
--- app/src/main/java/com/mergenc/barista/ui/customer/CustomerFragment.kt
+++ app/src/main/java/com/mergenc/barista/ui/customer/CustomerFragment.kt
@@ -11,16 +11,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.navArgs
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.WriterException
 import com.google.zxing.qrcode.QRCodeWriter
 import com.mergenc.barista.databinding.FragmentCustomerBinding
+import com.mergenc.barista.network.realtimedatabase.QRCodeID
+import com.mergenc.barista.ui.choosetype.ChooseTypeFragmentDirections
 import java.text.SimpleDateFormat
 import java.util.*
 
 class CustomerFragment : Fragment() {
     private var _binding: FragmentCustomerBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var database: DatabaseReference
+    private lateinit var qrId: String
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,17 +47,32 @@ class CustomerFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val args: CustomerFragmentArgs by navArgs()
+        qrId = args.qrId
+
+        insertIdToDatabase()
         generateQrCode()
+    }
+
+    private fun insertIdToDatabase() {
+        database = FirebaseDatabase.getInstance().getReference("qrCodeIds")
+        val qRCodeID = QRCodeID(qrId)
+
+        database.setValue(qRCodeID).addOnSuccessListener {
+            Log.d("Firebase", "QR Code ID inserted to database. ID: $qrId")
+        }.addOnFailureListener {
+            Log.e("Firebase", "QR Code ID could not be inserted to database")
+        }
     }
 
     @SuppressLint("SimpleDateFormat")
     private fun generateQrCode() {
         // random number int
-        val random = (1..999).random()
 
         val writer = QRCodeWriter()
         try {
-            val bitMatrix = writer.encode(random.toString(), BarcodeFormat.QR_CODE, 250, 250)
+            val bitMatrix = writer.encode(qrId, BarcodeFormat.QR_CODE, 250, 250)
             val width = bitMatrix.width
             val height = bitMatrix.height
             val bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
