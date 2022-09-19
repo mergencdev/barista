79f2847a4808a91f2816531615911588a42e6f7b
Mehmet Emin Ergenc <mergencdev@gmail.com>
2022-09-19 19:30:02 +0300

    -Cashier screen dialogs & developments

--- app/src/main/java/com/mergenc/barista/ui/cashier/CashierActivity.kt
+++ app/src/main/java/com/mergenc/barista/ui/cashier/CashierActivity.kt
@@ -1,8 +1,8 @@
 package com.mergenc.barista.ui.cashier
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -12,17 +12,25 @@ import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.*
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.android.gms.vision.CameraSource
 import com.google.android.gms.vision.Detector
 import com.google.android.gms.vision.barcode.Barcode
 import com.google.android.gms.vision.barcode.BarcodeDetector
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.database.*
+import com.google.firebase.ktx.Firebase
 import com.mergenc.barista.R
 import com.mergenc.barista.databinding.ActivityCashierBinding
+import com.mergenc.barista.network.analytics.AnalyticsEventConstants
+import com.mergenc.barista.network.realtimedatabase.SelectedPriceAmount
+import com.mergenc.barista.ui.MainActivity
 import java.io.IOException
+
 
 class CashierActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCashierBinding
@@ -33,9 +41,10 @@ class CashierActivity : AppCompatActivity() {
     private var scannedValue = ""
     private var selectedPrice: Int = 0
     private lateinit var qrCodeIdFirebase: String
-    private lateinit var qrCodeIdFromGeneration: String
 
     private lateinit var database: DatabaseReference
+    private lateinit var databasePayment: DatabaseReference
+    private lateinit var analytics: FirebaseAnalytics
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,7 +52,7 @@ class CashierActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        qrCodeIdFromGeneration = intent.getStringExtra("qrCodeId").toString()
+        analytics = Firebase.analytics
 
         if (ContextCompat.checkSelfPermission(
                 this@CashierActivity, android.Manifest.permission.CAMERA
@@ -57,6 +66,40 @@ class CashierActivity : AppCompatActivity() {
         val aniSlide: Animation =
             AnimationUtils.loadAnimation(this@CashierActivity, R.anim.scanner_animation)
         binding.barcodeLine.startAnimation(aniSlide)
+
+        check()
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun showPaymentMethodSuccessDialog(paymentMethod: String) {
+
+        val mDialogView = LayoutInflater.from(this@CashierActivity)
+            .inflate(R.layout.custom_payment_success_dialog, null)
+
+        val mBuilder =
+            AlertDialog.Builder(this@CashierActivity).setView(mDialogView).show()
+        mBuilder.window?.setBackgroundDrawableResource(android.R.color.transparent)
+
+        val buttonOkayPaymentSuccess =
+            mDialogView.findViewById(R.id.button_okay_payment_success) as Button
+        val textViewPaymentSuccess = mDialogView.findViewById(R.id.tv_payment_success) as TextView
+
+        when (paymentMethod) {
+            "wallet" -> {
+                textViewPaymentSuccess.text = "Müşteri Coin Wallet ile ödemeyi gerçekleştirdi."
+            }
+            "credit" -> {
+                textViewPaymentSuccess.text = "Müşteri kredi kartı ile ödemeyi gerçekleştirdi."
+            }
+        }
+
+        buttonOkayPaymentSuccess.setOnClickListener {
+            mBuilder.dismiss()
+
+            finish()
+            startActivity(Intent(this, MainActivity::class.java))
+            finishAffinity()
+        }
     }
 
     private fun retrieveIdFromDatabase() {
@@ -77,6 +120,7 @@ class CashierActivity : AppCompatActivity() {
         }
     }
 
+    @SuppressLint("SetTextI18n")
     private fun showPriceDialog() {
         val mDialogView = LayoutInflater.from(this@CashierActivity)
             .inflate(R.layout.custom_price_dialog, null)
@@ -90,6 +134,8 @@ class CashierActivity : AppCompatActivity() {
         val radioGroupPrice = mDialogView.findViewById<RadioGroup>(R.id.rg_price)
         val buttonOkay = mDialogView.findViewById<Button>(R.id.button_okay)
         val buttonCancel = mDialogView.findViewById<Button>(R.id.button_cancel)
+        val progressBar = mDialogView.findViewById<ProgressBar>(R.id.progressBarPayment)
+        val cardViewPayment = mDialogView.findViewById<CardView>(R.id.cwPayment)
 
         radioGroupPrice.setOnCheckedChangeListener { _, checkedId ->
             val radio: RadioButton = mDialogView.findViewById(checkedId)
@@ -113,6 +159,60 @@ class CashierActivity : AppCompatActivity() {
                 }
             }
         }
+
+        buttonOkay.setOnClickListener {
+            cardViewPayment.visibility = View.INVISIBLE
+            progressBar.visibility = View.VISIBLE
+
+            if (editTextNumber.visibility == View.VISIBLE) {
+                selectedPrice = editTextNumber.text.toString().toInt()
+            }
+
+            database = FirebaseDatabase.getInstance().getReference("price")
+            val selectedPriceAmount = SelectedPriceAmount(selectedPrice.toString())
+
+            database.setValue(selectedPriceAmount).addOnSuccessListener {
+                Log.d("Firebase", "Selected price inserted to database: $selectedPrice")
+
+            }.addOnFailureListener {
+                Log.e(
+                    "Firebase",
+                    "Selected price could not be inserted to database: $selectedPrice"
+                )
+            }
+
+            // Log firebase analytics event
+            val params = Bundle()
+            params.putInt(
+                AnalyticsEventConstants.CASHIER.PRICE_DIALOG.PRICE_DIALOG_PRICE,
+                selectedPrice
+            )
+            analytics.logEvent(
+                AnalyticsEventConstants.CASHIER.PRICE_DIALOG.PRICE_DIALOG_CONFIRM,
+                params
+            )
+
+            /*val params = Bundle()
+            params.putInt(AnalyticsEventConstants.CASHIER.PRICE_DIALOG.PRICE_DIALOG_PRICE, selectedPrice)
+            analytics.logEvent("selected_price", params)*/
+        }
+    }
+
+    private fun check() {
+        databasePayment = FirebaseDatabase.getInstance().getReference("paymentMethod")
+        databasePayment.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val selectedPaymentMethod = snapshot.child("selectedPaymentMethod").value.toString()
+                if (selectedPaymentMethod != "null") {
+                    showPaymentMethodSuccessDialog(selectedPaymentMethod)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.e("Firebase", "Payment method could not be retrieved from database")
+            }
+        })
+
     }
 
     private fun setupControls() {
@@ -171,6 +271,16 @@ class CashierActivity : AppCompatActivity() {
                         cameraSource.stop()
                         binding.barcodeLine.clearAnimation()
                         Log.d("QRCode", "QR Code: $scannedValue")
+
+                        val params = Bundle()
+                        params.putInt(
+                            AnalyticsEventConstants.CASHIER.QR_CODE_SCANNING.QR_CODE_VALUE,
+                            scannedValue.toInt()
+                        )
+                        analytics.logEvent(
+                            AnalyticsEventConstants.CASHIER.QR_CODE_SCANNING.QR_CODE_SCANNED,
+                            params
+                        )
 
                         retrieveIdFromDatabase()
                     }
