7ec577e3aef1db3d0bfc80c96e0a11b797cd539f
Mehmet Emin Ergenc <mergencdev@gmail.com>
2022-09-19 19:30:59 +0300

    -Random seed bugfix

--- app/src/main/java/com/mergenc/barista/ui/choosetype/ChooseTypeFragment.kt
+++ app/src/main/java/com/mergenc/barista/ui/choosetype/ChooseTypeFragment.kt
@@ -1,5 +1,6 @@
 package com.mergenc.barista.ui.choosetype
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -9,6 +10,8 @@ import android.view.ViewGroup
 import androidx.navigation.findNavController
 import com.mergenc.barista.databinding.FragmentChooseTypeBinding
 import com.mergenc.barista.ui.cashier.CashierActivity
+import java.time.Instant
+import kotlin.random.Random
 
 class ChooseTypeFragment : Fragment() {
     private var _binding: FragmentChooseTypeBinding? = null
@@ -29,17 +32,17 @@ class ChooseTypeFragment : Fragment() {
         _binding = null
     }
 
+    @SuppressLint("NewApi")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.buttonCashier.setOnClickListener {
-            qrId = (1..999).random().toString()
             val intent = Intent(activity, CashierActivity::class.java)
-            intent.putExtra("qrId", qrId)
             activity?.startActivity(intent)
         }
 
         binding.buttonCustomer.setOnClickListener {
+            qrId = Random(Instant.now().nano).nextInt(1,999).toString()
             val action =
                 ChooseTypeFragmentDirections.actionChooseTypeFragmentToCustomerFragment(qrId)
             it.findNavController().navigate(action)
